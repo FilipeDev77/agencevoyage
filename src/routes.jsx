d1f20d8ce5bbefc992fdfa85bda2abd927404079
FilipeDev77 <filipemoreiravieira@gmail.com>
2024-12-12 18:47:12 +0100

    ajout des reservations

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardView from './Home/view/dashboardView';
 import Card from './Voyages/view/cardview';
 import ReservationView from './Reservation/view/reservationView';
-import Infoscityview from './Voyages/view/infoscityview';
+
 
 function AppRoutes() {
   const [reservations, setReservations] = useState([]);
@@ -18,7 +18,7 @@ function AppRoutes() {
         <Route path="/" element={<DashboardView />} />
         <Route path="/voyages" element={<Card addReservation={addReservation} />} />
         <Route path="/reservations" element={<ReservationView reservations={reservations} />} />
-        <Route path="/voyages/city/:city" element={<Infoscityview />} />
+      
       </Routes>
     
   );
